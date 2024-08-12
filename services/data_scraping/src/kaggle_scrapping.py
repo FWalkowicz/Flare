fc180c6d5f1355b534399d641ac15d00ca3dc0c2
FWalkowicz <fillio00@wp.pl>
2024-08-12 10:13:28 +0200

    new endpoints: download, search

--- services/data_scraping/src/kaggle_scrapping.py
+++ services/data_scraping/src/kaggle_scrapping.py
@@ -1,5 +1,4 @@
 import os
-
 import init.kaggle_init
 from kaggle.api.kaggle_api_extended import KaggleApi
 import pandas as pd
@@ -7,14 +6,27 @@ import pandas as pd
 
 class KaggleScrapping:
     """
-    A class used to interact with Kaggle's API.
+    A class used to interact with Kaggle's API for searching and downloading datasets, and processing them.
 
     Methods
     -------
     search(dataset_name: str) -> pd.DataFrame:
         Searches Kaggle for datasets matching the provided name and filters them to include only those with .csv files.
+
+    download(dataset_name: str, file_name: str) -> pd.DataFrame:
+        Downloads a specified CSV file from a Kaggle dataset and processes it by removing rows with missing values.
+
+    cut(data: pd.DataFrame, indexes_to_drop: list) -> pd.DataFrame:
+        Drops specified columns from a DataFrame by their index positions.
     """
+
     def __init__(self):
+        """
+        Initializes the KaggleScrapping class by setting up a connection to the Kaggle API.
+
+        This constructor attempts to authenticate with the Kaggle API using the credentials
+        set up in the environment. If authentication fails, a NameError is caught, and a message is printed.
+        """
         try:
             self.api = KaggleApi()
             self.api.authenticate()
@@ -23,10 +35,16 @@ class KaggleScrapping:
 
     def search(self, dataset_name: str) -> pd.DataFrame:
         """
-        Search for dataset on Kaggle with provided dataset name
+        Searches for datasets on Kaggle that match the provided dataset name or keyword.
 
-        :param dataset_name: (str) The name or key word to search on Kaggle
-        :return: (pd.DataFrame) A dataframe containing dataset name and files
+        This method retrieves a list of datasets matching the search criteria and filters
+        them to include only those datasets that contain .csv files. The results are returned
+        in a pandas DataFrame.
+
+        :param dataset_name: str
+            The name or keyword to search for on Kaggle.
+        :return: pd.DataFrame
+            A DataFrame containing the dataset reference, subtitle, and a list of .csv files available in each dataset.
         """
         dataset_list = self.api.dataset_list(search=dataset_name)
         datasets_with_csv_files = []
@@ -46,19 +64,36 @@ class KaggleScrapping:
 
         return df_with_csv_files
 
-    def download(self, dataset_name: str, file_name: str):
+    def download(self, dataset_name: str, file_name: str) -> pd.DataFrame:
+        """
+        Downloads a specific CSV file from a Kaggle dataset, saves it locally, and processes it by removing any rows with missing values.
+
+        The file is downloaded to a 'utils' directory, renamed to 'dataset.csv', and then loaded into a pandas DataFrame.
+        The DataFrame is cleaned by removing any rows that contain missing values.
+
+        :param dataset_name: str
+            The reference name of the dataset on Kaggle.
+        :param file_name: str
+            The name of the CSV file within the dataset to download.
+        :return: pd.DataFrame
+            A cleaned pandas DataFrame containing the data from the downloaded CSV file.
+        """
         self.api.dataset_download_file(dataset_name, file_name, path='./utils')
-        os.rename('./utils/'+file_name, './utils/dataset.csv')
+        os.rename('./utils/' + file_name, './utils/dataset.csv')
         dataset = pd.read_csv('./utils/dataset.csv').dropna()
         return dataset
 
     @staticmethod
     def cut(data: pd.DataFrame, indexes_to_drop: list) -> pd.DataFrame:
+        """
+        Removes specified columns from the given DataFrame based on column index positions.
+
+        :param data: pd.DataFrame
+            The DataFrame from which columns will be removed.
+        :param indexes_to_drop: list
+            A list of column index positions to drop from the DataFrame.
+        :return: pd.DataFrame
+            A DataFrame with the specified columns removed.
+        """
         dataset = data.drop(data.columns[indexes_to_drop], axis=1)
         return dataset
-
-
-if __name__ == '__main__':
-    scraper = KaggleScrapping()
-    print(scraper.search('pokemon'))
-    print(scraper.download('abcsds/pokemon', 'Pokemon.csv').head(25))
--- services/data_scraping/src/main.py
+++ services/data_scraping/src/main.py
@@ -1,13 +1,19 @@
 import json
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from kaggle_scrapping import KaggleScrapping
 
+# Initialize the FastAPI app
 app = FastAPI()
-scrapper = KaggleScrapping()
+
+# Initialize the KaggleScrapping instance to interact with Kaggle API
+scraper = KaggleScrapping()
+
+# Define the origins that are allowed to make cross-origin requests (CORS)
 cors_origins = ["*"]
 
+# Add CORS middleware to the FastAPI app to allow requests from any origin
 app.add_middleware(
     CORSMiddleware,
     allow_origins=cors_origins,
@@ -19,7 +25,48 @@ app.add_middleware(
 
 @app.get("/show_datasets/{name}")
 def show_datasets(name: str):
-    datasets = scrapper.search(name)
-    datasets_json = datasets.to_json(orient="records")
+    """
+    Endpoint to search for datasets on Kaggle by name and return the results in JSON format.
+
+    :param name: str
+        The name or keyword to search for datasets on Kaggle.
+    :return: JSON
+        A JSON list of datasets that match the search criteria, including the dataset reference,
+        subtitle, and available .csv files.
+    """
+    datasets_list = scraper.search(name)
+    datasets_json = datasets_list.to_json(orient="records")
     parsed = json.loads(datasets_json)
+    return parsed
+
+
+class Dataset_to_download(BaseModel):
+    """
+    Data model to define the structure of the data required for downloading a dataset.
+
+    Attributes
+    ----------
+    ref_name : str
+        The reference name of the dataset on Kaggle.
+    file_name : str
+        The name of the specific file within the dataset to download.
+    """
+    ref_name: str
+    file_name: str
+
+
+@app.post("/download")
+def download(dataset_info: Dataset_to_download):
+    """
+    Endpoint to download a specified CSV file from a Kaggle dataset and return the first 25 rows in JSON format.
+
+    :param dataset_info: Dataset_to_download
+        A Pydantic model containing the reference name of the Kaggle dataset and the file name to download.
+    :return: JSON
+        A JSON list of the first 25 rows of the downloaded dataset.
+    """
+    print(dataset_info)
+    dataset = scraper.download(dataset_name=dataset_info.ref_name, file_name=dataset_info.file_name).head(25)
+    dataset_json = dataset.to_json(orient="records")
+    parsed = json.loads(dataset_json)
     return parsed
